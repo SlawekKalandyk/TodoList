676c9ccd326b49d02fb8efb7e7ffeb35fbe52cd5
Slawomir Kalandyk <slawomir.kalandyk@gmail.com>
2026-05-17 06:42:05 +0200

    fix: Fix Rider IDE reported errors

--- src/TodoList.App/Program.cs
+++ src/TodoList.App/Program.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace TodoList.App;
 
-sealed class Program
+internal sealed class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
